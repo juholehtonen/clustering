040d7c2112c20843787004fd59232a6a39e14efc
Juho Lehtonen <juho.lehtonen@csc.fi>
2017-10-30 10:49:33 +0200

    Modify to take list of used fields. Write processed data preview file.

--- preprocess.py
+++ preprocess.py
@@ -4,8 +4,8 @@
 #
 # Usage: python preprocessing.py <label>
 ##################################################################
-import numpy as np
 import cPickle
+import pprint
 import re
 import sys
 
@@ -24,26 +24,26 @@ abstract_line = r'Abstrakti'
 keyword_publisher_line = r'Avainsana \(KeywordPlus\):  (.*)'
 keyword_line = r'Avainsana \(tekijät\):  (.*)'
 reference_line = r'Lähde: (.*)'
-closing_line = r' * '
+closing_line = r' \* '
+
+#used_fields = ['id', 'issn', 'title', 'abstract', 'keyword_publisher', 'keyword', 'reference']
+used_fields = ['title', 'keyword', 'reference']
 
 
 def dataset(batch_size):
     datasets = []
-    data = {'keyword_publisher': '',
-            'keyword': '',
-            'reference': ''}
-            #'discipline': '',}
+    current = {}
     k = 0
     with open(filename) as f:
         for line in f:
             m = re.match(opening_line, line)
-            if m:
-                data['id'] = m.group('identifier')
+            if m and 'id' in used_fields:
+                current['id'] = m.group('identifier')
                 continue
 
             m = re.match(issn_line, line)
-            if m:
-                data['issn'] = m.group(1)
+            if m and 'issn' in used_fields:
+                current['issn'] = m.group(1)
                 continue
 
             # m = re.match(discipline_line, line)
@@ -51,48 +51,58 @@ def dataset(batch_size):
             #     data['discipline'] = ' '.join([data['discipline'], m.group(1).replace(' & ', '_&_')])
             #     continue
 
-            # Note: remeber to parse two character words too 'chromosome arm 3p'
+            # Note: remember to parse two character words too 'chromosome arm 3p'
             m = re.match(title_line, line)
-            if m:
-                data['title'] = m.group(1)
+            if m and 'title' in used_fields:
+                current['title'] = m.group(1)
                 continue
 
-            # Note: remeber to parse two character words too 'chromosome arm 3p'
+            # Note: remember to parse two character words too 'chromosome arm 3p'
             m = re.match(abstract_line, line)
-            if m:
-                data['abstract'] = f.next()
+            if m and 'abstract' in used_fields:
+                current['abstract'] = f.next()
                 continue
 
             m = re.match(keyword_publisher_line, line)
-            if m:
-                data['keyword_publisher'] = ' '.join([data['keyword_publisher'], m.group(1).replace(' ', '_')])
+            if m and 'keyword_publisher' in used_fields:
+                value = current.get('keyword_publisher')
+                if value:
+                    current['keyword_publisher'] = ' '.join([value,
+                                                             m.group(1).replace(' ', '_')])
+                else:
+                    current['keyword_publisher'] = m.group(1).replace(' ', '_')
                 continue
 
             m = re.match(keyword_line, line)
-            if m:
-                data['keyword'] = ' '.join([data['keyword'], m.group(1).replace(' ', '_')])
+            if m and 'keyword' in used_fields:
+                value = current.get('keyword')
+                if value:
+                    current['keyword'] = ' '.join([value, m.group(1).replace(' ', '_')])
+                else:
+                    current['keyword'] = m.group(1).replace(' ', '_')
                 continue
 
             m = re.match(reference_line, line)
-            if m:
+            if m and 'reference' in used_fields:
                 ref = re.sub('(\d+) /.*', '\\1', m.group(1))
-                data['reference'] = ' '.join([data['reference'], ref.replace(' ', '_')])
+                value = current.get('reference')
+                if value:
+                    current['reference'] = ' '.join([value, ref.replace(' ', '_')])
+                else:
+                    current['reference'] = ref.replace(' ', '_')
                 continue
 
             m = re.match(closing_line, line)
             if m:
-                datasets.append(data.copy())
-                data['keyword_publisher'] = ''
-                data['keyword'] = ''
-                data['reference'] = ''
+                datasets.append(current.copy())
+                current = {}
                 k += 1
             if k >= batch_size:
                 break
     return datasets
 
 data = dataset(batch_size=b_size)
-#import pprint
-#pprint.pprint(data[:3])
 with open('../data/{0}-preprocessed.txt'.format(str(b_size)), 'w') as handle:
     cPickle.dump(data, handle)
-#np.savetxt('data/{0}-preprocessed.txt'.format(label), data)
+with open('../data/preprocessed-preview.txt', 'w') as handle:
+    handle.write(pprint.pformat(data[:5]))
