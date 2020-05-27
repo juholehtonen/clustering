a75346debf2a39163a01db0d3d07dc375e0987c9
Juho Lehtonen <juho.lehtonen@gmail.com>
2020-05-27 11:27:26 +0300

    Write chapter 5, initial results for manually annotated data

---
+++ src/calculate_results.py
@@ -0,0 +1,14 @@
+# coding=UTF8
+##################################################################
+# Compare and calculate clustering results
+#
+# Usage: python calculate_results.py <args>
+##################################################################
+
+# Tuodaan ensin nykyinen "oikea" jako
+
+
+# Tuodaan laskettu kolmen klusteri
+
+# Tarvitaan objektien yksilöivät tunnisteet!
+# Lasketaan herkkyys (recall/sensitivity)
