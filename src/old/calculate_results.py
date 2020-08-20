8b93c8fc7886233ffe017075abb2e4968c50e779
Juho Lehtonen <juho.lehtonen@gmail.com>
2020-08-21 01:08:39 +0300

    Move obsolete stuff to /old

--- src/old/calculate_results.py
+++ src/old/calculate_results.py
@@ -4,8 +4,13 @@
 #
 # Usage: python calculate_results.py <args>
 ##################################################################
+import pandas as pd
 
 # Tuodaan ensin nykyinen "oikea" jako
+model_dir = '../models/'
+truth_file = model_dir + 'groundtruth_labels_final.csv'
+
+labels_true = pd.read_csv(truth_file, index_col=0)
 
 
 # Tuodaan laskettu kolmen klusteri
